cc0d37d70d349d313d2b93ceff8d965e867e2ac7
PavelDulskij <paveldulskij06@gmail.com>
2025-11-14 17:11:48 +0300

    Update TextComponentType.java

--- src/main/java/com/innowise/taskcomposite/component/TextComponentType.java
+++ src/main/java/com/innowise/taskcomposite/component/TextComponentType.java
@@ -1,5 +1,5 @@
 package com.innowise.taskcomposite.component;
 
 public enum TextComponentType {
-     TEXT, PARAGRAPH, SENTENCE, LEXEME, WORD, SYMBOL
+     PARAGRAPH, SENTENCE, LEXEME, WORD, SYMBOL
 }
